13cbf4ddedc95747f3af4e078e9b2dbd15f54785
Nabiilah527 <2411102441202@umkt.ac.id>
2025-12-17 15:44:53 +0800

    setelah refactori

---
+++ registrasi_sesudah.py
@@ -0,0 +1,56 @@
+from abc import ABC, abstractmethod
+
+class Mahasiswa:
+    def __init__(self, nama, sks, prasyarat_lengkap):
+        self.nama = nama
+        self.sks = sks
+        self.prasyarat_lengkap = prasyarat_lengkap
+
+# --- IMPLEMENTASI SOLID ---
+
+# 1. Kontrak Abstraksi (DIP) 
+class IValidationRule(ABC):
+    @abstractmethod
+    def validate(self, mhs: Mahasiswa) -> bool:
+        pass
+
+# 2. Implementasi SRP 
+class SKSValidator(IValidationRule):
+    def validate(self, mhs: Mahasiswa) -> bool:
+        if mhs.sks <= 24:
+            return True
+        print(f"Log: SKS {mhs.sks} melampaui batas.")
+        return False
+
+class PrerequisiteValidator(IValidationRule):
+    def validate(self, mhs: Mahasiswa) -> bool:
+        if mhs.prasyarat_lengkap:
+            return True
+        print("Log: Prasyarat belum lengkap.")
+        return False
+
+# 3. Koordinator yang fleksibel (OCP & Dependency Injection) 
+class RegistrationService:
+    def __init__(self, rules: list):
+        self.rules = rules 
+
+    def run(self, mhs: Mahasiswa):
+        print(f"--- Memproses Registrasi: {mhs.nama} ---")
+        success = True
+        for rule in self.rules:
+            if not rule.validate(mhs):
+                success = False
+        
+        if success:
+            print("Status: Registrasi Diterima!")
+        else:
+            print("Status: Registrasi Ditolak!")
+
+# Simulasi Eksekusi
+if __name__ == "__main__":
+    mhs_budi = Mahasiswa("Budi", 20, True)
+    
+    aturan = [SKSValidator(), PrerequisiteValidator()]
+    
+    app = RegistrationService(aturan)
+    app.run(mhs_budi)
